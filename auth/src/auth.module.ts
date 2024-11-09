62422151151248086385276865da22b59601cc99
Garra <mateusdepaula3@gmail.com>
2024-11-08 23:09:06 -0300

    feat(auth) adding configService to auth module

--- auth/src/auth.module.ts
+++ auth/src/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from './database/prisma.service';
-import { ConfigModule, ConfigService, ConfigType } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
@@ -13,46 +13,53 @@ import { LoggerModule } from './shared/logger/logger.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './shared/interceptors/loggin.interceptor';
 import { AuthQueue } from './queues/auth-queue';
+import rmqConfig from './config/rmq.config';
+import tcpConfig from './config/tcp.config';
 
 @Module({
   imports: [
     LoggerModule,
     JwtModule.register({}),
-    ConfigModule.forRoot({ load: [jwtConfig], isGlobal: true }),
-    ClientsModule.register([
+    ConfigModule.forRoot({ load: [jwtConfig, rmqConfig, tcpConfig], isGlobal: true }),
+    ClientsModule.registerAsync([
       {
         name: 'AUTH_QUEUE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://user:password@localhost:5672'],
-          queue: 'authQueue', 
-          queueOptions: {
-            durable: true,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('rmq.url')], // Use ConfigService para obter a URL
+            queue: 'authQueue',
+            queueOptions: {
+              durable: true,
+            },
           },
-        },
+        }),
+        inject: [ConfigService],
       },
-    ]),
-    ClientsModule.register([
       {
         name: 'MAILER_QUEUE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://user:password@localhost:5672'],
-          queue: 'mailerQueue', 
-          queueOptions: {
-            durable: true,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('rmq.url')], // Use ConfigService para obter a URL
+            queue: 'mailerQueue',
+            queueOptions: {
+              durable: true,
+            },
           },
-        },
+        }),
+        inject: [ConfigService],
       },
-    ]),
-    ClientsModule.register([
       {
         name: 'USER_SERVICE',
-        transport: Transport.TCP,
-        options: {
-          host: '127.0.0.1',
-          port: 3001,
-        },
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configService.get<string>('tcp.authServiceHost'), // Use ConfigService para obter o host
+            port: configService.get<number>('tcp.authServicePort'),  // Use ConfigService para obter a porta
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
   ],
@@ -70,4 +77,4 @@ import { AuthQueue } from './queues/auth-queue';
     },
   ],
 })
-export class AuthModule { }
+export class AuthModule {}
